c6705d607dd27021846e93cd8b1094952309aac5
G-who0212 <gwho990212@gmai.com>
2024-12-19 17:42:10 +0900

    jenkins file deploy to ec2 again

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,33 +1,48 @@
-node {
-  stage('Clone repository') {
-    // SCM(Source Control Management)을 사용하여 코드 저장소 클론
-    checkout scm
-  }
-  
-  stage('Build image') {
-    // Docker 이미지를 빌드하고 태그를 BUILD_NUMBER로 지정
-    app = docker.build("gwho0212/countfit-backend:$BUILD_NUMBER")
-  }
-  
-  stage('Push image') {
-    // Docker Hub에 인증하고 이미지를 푸시
-    docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
-      app.push("${env.BUILD_NUMBER}") // 태그별로 푸시
-      app.push("latest") // latest 태그로도 푸시
-    }
-  }
-  stage('Deploy to EC2') {
-        steps {
-            sshagent(['countfit-backend-ec2-ssh-key']) { // EC2 SSH Credential ID
-                sh '''
-                ssh -o StrictHostKeyChecking=no ubuntu@3.35.51.27 << EOF
-                docker pull gwho0212/countfit-backend:latest
-                docker stop countfit-backend || true
-                docker rm countfit-backend || true
-                docker run -d --name countfit-backend -p 80:80 gwho0212/countfit-backend:latest
-                EOF
-                '''
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone repository') {
+            steps {
+                // SCM(Source Control Management)을 사용하여 코드 저장소 클론
+                checkout scm
             }
         }
-  }
+
+        stage('Build image') {
+            steps {
+                script {
+                    // Docker 이미지를 빌드하고 태그를 BUILD_NUMBER로 지정
+                    app = docker.build("gwho0212/countfit-backend:${env.BUILD_NUMBER}")
+                }
+            }
+        }
+
+        stage('Push image') {
+            steps {
+                script {
+                    // Docker Hub에 인증하고 이미지를 푸시
+                    docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+                        app.push("${env.BUILD_NUMBER}") // 태그별로 푸시
+                        app.push("latest") // latest 태그로도 푸시
+                    }
+                }
+            }
+        }
+
+        stage('Deploy to EC2') {
+            steps {
+                sshagent(['countfit-backend-ec2-ssh-key']) { // EC2 SSH Credential ID
+                    sh '''
+                    ssh -o StrictHostKeyChecking=no ubuntu@3.35.51.27 << EOF
+                    docker pull gwho0212/countfit-backend:latest
+                    docker stop countfit-backend || true
+                    docker rm countfit-backend || true
+                    docker run -d --name countfit-backend -p 80:80 gwho0212/countfit-backend:latest
+                    EOF
+                    '''
+                }
+            }
+        }
+    }
 }
